6101e9082de605bd622022ebfc498fc27952a15d
nikson.palokaj <nikson.palokaj@divcom.ch>
2019-05-20 10:08:01 +0200

    création du plugin

--- ico-plugin/ico-plugin.php
+++ ico-plugin/ico-plugin.php
@@ -3,10 +3,10 @@
 Plugin Name: Mon Premier Plugin
 */
 //Fonction qui affiche la balise meta
-function mon_plugin_meta_keywords() {
+function ico_plugin_meta_keywords() {
     echo '<meta name="keywords" content="HTML,CSS,XML,JavaScript">';
 }
 //Ajout d'une action sur 'wp_head' qui appellera mon_plugin_meta_keywords()
-add_action('wp_head', 'mon_plugin_meta_keywords' );
+add_action('wp_head', 'ico_plugin_meta_keywords' );
 
 
