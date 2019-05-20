6f4ada6abe29073940d23ec2c26670edd48aab4d
nikson.palokaj <nikson.palokaj@divcom.ch>
2019-05-20 10:51:07 +0200

    test

--- ico-plugin/ico-plugin.php
+++ ico-plugin/ico-plugin.php
@@ -11,21 +11,16 @@ add_action('wp_head', 'mon_plugin_meta_keywords' );
 
 
 function ico_plugin_vador_shortcode($atts, $content = "") {
-    //Si contenu vide
-    if (empty( $content )) {
-        $content = 'Luke';
-    }
-    return "<blockquote>" . $content . ", Je suis ton père !</blockquote>";
+
+    return '<img style="background-color:red; padding:20px; border-radius:20px;"
+src="http://monsite.com/wp-content/plugins/ico-plugin/img/'.$content.'.png"
+ alt="wordpress" /%gt;';
 }
 
-//Fonction de rappel qui retourne la célèbre citation de maître Yoda
-function ico_plugin_yoda_shortcode() {
-    return "<blockquote>Que la force soit avec toi jeune padawan !</blockquote>";
-}
 
 //Enregistre les shortcodes du plugin
 function ico_plugin_register_shortcode() {
     add_shortcode( 'yoda', 'mon_plugin_yoda_shortcode' );
-    add_shortcode( 'vador', 'mon_plugin_vador_shortcode' );
+
 }
 add_action( 'init', 'mon_plugin_register_shortcode' );
