4cfea22f5aadb1e935ff689e99f6c4ac8e3960ab
nikson.palokaj <nikson.palokaj@divcom.ch>
2019-05-20 11:11:07 +0200

    test

--- ico-plugin/ico-plugin.php
+++ ico-plugin/ico-plugin.php
@@ -10,17 +10,28 @@ function ico_plugin_meta_keywords() {
 add_action('wp_head', 'mon_plugin_meta_keywords' );
 
 
-function ico_plugin_vador_shortcode($atts, $content = "") {
+function ico_plugin_shortcode($atts, $content = "") {
 
-    return '<img style="background-color:red; padding:20px; border-radius:20px;"
+   /* return '<img style="background-color:red; padding:20px; border-radius:20px;"
 src="http://monsite.com/wp-content/plugins/ico-plugin/img/'.$content.'.png"
- alt="wordpress" /%gt;';
+ alt="wordpress" /%gt;';*/
+
+$plugin_url= plugins_url();
+
+   return '<img style="background-color:red; padding:20px; border-radius:20px;"
+src="'.$plugin_url.'/ico-plugin/img/'.$content.'.png"
+ alt=\"wordpress\" />';
+
+
+
+
+
 }
 
 
 //Enregistre les shortcodes du plugin
 function ico_plugin_register_shortcode() {
-    add_shortcode( 'ico', 'ico_plugin_yoda_shortcode' );
+    add_shortcode( 'ico', 'ico_plugin_shortcode' );
 
 }
-add_action( 'init', 'mon_plugin_register_shortcode' );
+add_action( 'init', 'ico_plugin_register_shortcode' );
